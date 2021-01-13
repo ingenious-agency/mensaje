a83b3ade00a2009ee3feec2aa091edfed2d4323d
Gabriel Chertok <gchertok@gmail.com>
2021-01-13 14:33:38 -0300

    Fix website url for slack response

--- app/api/slack.ts
+++ app/api/slack.ts
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default async function slack(req: NextApiRequest, res: NextApiResponse) {
-  res.write(
-    `👉 <http://${process.env.WEBSITE_URL}/messages/new?channel=${req.body.channel_id}|Follow me>`
-  )
+  res.write(`👉 <${process.env.WEBSITE_URL}/messages/new?channel=${req.body.channel_id}|Follow me>`)
   res.end()
 }
