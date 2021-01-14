e60c871b1601e3d3cd8e6fd8558ccc22c776ab65
Gabriel Chertok <gchertok@gmail.com>
2021-01-14 16:14:53 -0300

    Clean up reactions

--- app/messages/components/reactions.tsx
+++ app/messages/components/reactions.tsx
@@ -1,9 +1,9 @@
-import ReactionList from "app/reactions/components/reaction-list"
 import createReaction from "app/reactions/mutations/createReaction"
 import deleteReaction from "app/reactions/mutations/deleteReaction"
 import getReactions from "app/reactions/queries/getReactions"
-import { emojis } from "app/reactions/types"
+import { EmojiKey, emojis } from "app/reactions/types"
 import { useMutation, useQuery } from "blitz"
+import { groupBy, pipe, pick, size, compact, curry } from "lodash/fp"
 
 const getReactionsCondition = ({ messageId, userId, include }) => ({
   where: {
@@ -30,67 +30,105 @@ export default function Reactions({ messageId, userId }: ReactionsProps) {
   )
   const [createReactionMutation] = useMutation(createReaction)
   const [deleteReactionMutation] = useMutation(deleteReaction)
-  return (
-    <>
-      {
-        <ReactionList
-          mine={myReactions?.reactions}
-          others={othersReactions?.reactions}
-          onCreate={async (emoji) => {
-            setQueryData(
-              {
-                ...myReactions,
-                reactions: myReactions.reactions.concat([
-                  {
-                    emoji: emoji,
-                    alt: emojis[emoji],
-                    id: "",
-                    messageId,
-                    userId,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                  },
-                ]),
-                count: myReactions.count + 1,
-              },
-              { refetch: false }
-            )
-            try {
-              await createReactionMutation({
-                data: {
-                  message: { connect: { id: messageId } },
-                  emoji: emoji,
-                  alt: emojis[emoji],
-                },
-              })
-            } catch (e) {
-              console.error(e)
-            }
-            await refetch()
-          }}
-          onRemove={async (emoji) => {
-            const reaction = myReactions.reactions.find((reaction) => reaction.emoji === emoji)
-            if (reaction) {
-              setQueryData(
-                {
-                  ...myReactions,
-                  reactions: myReactions.reactions.filter((item) => item.id !== reaction?.id),
-                  count: myReactions.count - 1,
-                },
-                { refetch: false }
-              )
-              try {
-                await deleteReactionMutation({
-                  where: { id: reaction.id },
-                })
-              } catch (e) {
-                console.error(e)
-              }
-              await refetch()
-            }
-          }}
-        />
+
+  const count = (key) =>
+    pipe(
+      compact,
+      groupBy("emoji"),
+      pick(key),
+      size,
+      curry((size: number) => (size > 0 ? `${size}` : null))
+    )
+
+  const onCreate = async (emoji) => {
+    try {
+      const queryData = {
+        ...myReactions,
+        reactions: myReactions.reactions.concat([
+          {
+            emoji: emoji,
+            alt: emojis[emoji],
+            id: new Date().getTime().toString(),
+            messageId,
+            userId,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ]),
+        count: myReactions.count + 1,
       }
-    </>
+      setQueryData(queryData, { refetch: false })
+      await createReactionMutation({
+        data: {
+          message: { connect: { id: messageId } },
+          emoji: emoji,
+          alt: emojis[emoji],
+        },
+      })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      await refetch()
+    }
+  }
+
+  const onRemove = async (emoji) => {
+    const reaction = myReactions.reactions.find((reaction) => reaction.emoji === emoji)
+    if (!reaction) return
+
+    try {
+      const queryData = {
+        ...myReactions,
+        reactions: myReactions.reactions.filter((item) => item.id !== reaction?.id),
+        count: myReactions.count - 1,
+      }
+      setQueryData(queryData, { refetch: false })
+      await deleteReactionMutation({
+        where: { id: reaction.id },
+      })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      await refetch()
+    }
+  }
+
+  return (
+    <div className="flex">
+      {Object.keys(emojis).map((key: EmojiKey) => {
+        const mine = myReactions?.reactions ?? []
+        const others = othersReactions?.reactions ?? []
+        const amount = count(key)([...mine, ...others])
+        const selected = mine.map((reaction) => reaction.emoji).includes(key)
+        return (
+          <button
+            key={key}
+            style={{
+              width: selected ? 51 : 39,
+              height: 26,
+              borderRadius: selected ? 100 : 76.5,
+              outline: 0,
+            }}
+            className={`flex items-center justify-center mr-2 cursor-pointer hover:bg-gray-350 border-none ${
+              selected && "bg-gray-350"
+            }`}
+            onClick={() => {
+              if (myReactions.reactions?.map((reaction) => reaction.emoji).includes(key)) {
+                onRemove(key)
+              } else {
+                onCreate(key)
+              }
+            }}
+          >
+            {key}{" "}
+            {amount && (
+              <small style={{ color: "#4D4D4D" }} className="text-xs ml-2">
+                {amount}
+              </small>
+            )}
+          </button>
+        )
+      })}
+    </div>
   )
 }
