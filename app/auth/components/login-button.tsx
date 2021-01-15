f6a9df4077c1eed27f7c1318671e85d021a29d02
Gabriel Chertok <gchertok@gmail.com>
2021-01-15 13:36:37 -0300

    Add `redirectUrl` to login

--- app/auth/components/login-button.tsx
+++ app/auth/components/login-button.tsx
@@ -1,13 +1,18 @@
 import { useCurrentUser } from "app/hooks/useCurrentUser"
-import { useMutation } from "blitz"
+import { useMutation, useRouter } from "blitz"
 import logout from "app/auth/mutations/logout"
 
 export default function LoginButton() {
   const user = useCurrentUser()
+  const router = useRouter()
   const [logoutMutation] = useMutation(logout)
   return (
     <div className="text-white absolute top-2 right-2 text-xs">
-      {user ? `${user?.name || user?.email} | ` : <a href="/api/auth/slack">Login</a>}
+      {user ? (
+        `${user?.name || user?.email} | `
+      ) : (
+        <a href={`/api/auth/slack?redirectUrl=${router.asPath}`}>Login</a>
+      )}
       {user && <button onClick={async () => await logoutMutation()}>Logout</button>}
     </div>
   )
