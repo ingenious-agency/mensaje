7c6f1150a3118dc197974d2482afda07f0c4da79
Gabriel Chertok <gchertok@gmail.com>
2021-01-15 16:31:41 -0300

    Add redirect uri

--- app/components/install-app.jsx
+++ app/components/install-app.jsx
@@ -1,7 +1,9 @@
 export default function InstallApp() {
   return (
     <div className="absolute h-screen w-screen bg-white flex items-center justify-center">
-      <a href="https://slack.com/oauth/v2/authorize?client_id=2756934506.1603903113093&scope=commands&user_scope=channels:read,chat:write,reactions:write,groups:read,mpim:read,im:read">
+      <a
+        href={`https://slack.com/oauth/v2/authorize?redirect_uri=${process.env.WEBSITE_URL}/&client_id=2756934506.1603903113093&scope=commands&user_scope=channels:read,chat:write,reactions:write,groups:read,mpim:read,im:read`}
+      >
         <img
           alt="Add to Slack"
           height="40"
