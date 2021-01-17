4a7e15a6e8ad8a8bd066b064c8ed40e0ca1f371c
Gabriel Chertok <gchertok@gmail.com>
2021-01-17 01:52:33 -0300

    Fix reaction creator

--- app/reactions/mutations/createReaction.ts
+++ app/reactions/mutations/createReaction.ts
@@ -1,11 +1,13 @@
 import { Ctx } from "blitz"
 import AddQueue from "app/api/reactions/add"
-import db, { Prisma } from "db"
+import db, { Prisma, User } from "db"
 
 type CreateReactionInput = Pick<Prisma.ReactionCreateArgs, "data">
 
 export default async function createReaction({ data }: CreateReactionInput, ctx: Ctx) {
   ctx.session.authorize()
+  const user = (await db.user.findUnique({ where: { id: ctx.session.userId } })) as User
+
   const existingReaction = await db.reaction.findFirst({
     where: {
       userId: ctx.session.userId,
@@ -17,15 +19,15 @@ export default async function createReaction({ data }: CreateReactionInput, ctx:
 
   const reaction = await db.reaction.create({
     data: { ...data, user: { connect: { id: ctx.session.userId } } },
-    include: { message: { include: { user: true } } },
+    include: { message: true },
   })
 
-  if (reaction.message?.slackTimeStamp && reaction.message.user?.slackAccessToken) {
+  if (reaction.message?.slackTimeStamp) {
     await AddQueue.enqueue({
       channel: reaction.message.slackChannelId,
       timestamp: reaction.message.slackTimeStamp,
       name: data.alt,
-      userToken: reaction.message.user?.slackAccessToken,
+      userToken: user.slackAccessToken,
     })
   }
 
--- app/reactions/mutations/deleteReaction.ts
+++ app/reactions/mutations/deleteReaction.ts
@@ -1,5 +1,5 @@
 import { AuthorizationError, Ctx } from "blitz"
-import db, { Prisma } from "db"
+import db, { Prisma, User } from "db"
 import getReaction from "app/reactions/queries/getReaction"
 import RemoveQueue from "app/api/reactions/remove"
 
@@ -7,20 +7,22 @@ type DeleteReactionInput = Pick<Prisma.ReactionDeleteArgs, "where">
 
 export default async function deleteReaction({ where }: DeleteReactionInput, ctx: Ctx) {
   ctx.session.authorize()
+  const user = (await db.user.findUnique({ where: { id: ctx.session.userId } })) as User
 
   const existingReaction = await getReaction({ where }, ctx)
   if (existingReaction.userId !== ctx.session.userId) throw new AuthorizationError()
+
   const reaction = await db.reaction.delete({
     where,
-    include: { message: { include: { user: true } } },
+    include: { message: true },
   })
 
-  if (reaction.message?.slackTimeStamp && reaction.message.user?.slackAccessToken) {
+  if (reaction.message?.slackTimeStamp) {
     await RemoveQueue.enqueue({
       channel: reaction.message.slackChannelId,
       timestamp: reaction.message.slackTimeStamp,
       name: reaction.alt,
-      userToken: reaction.message.user?.slackAccessToken,
+      userToken: user.slackAccessToken,
     })
   }
 
