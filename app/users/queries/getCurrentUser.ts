bca1a2e33ef2998ad255e17d90ee9e3a07665665
Gabriel Chertok <gchertok@gmail.com>
2021-03-04 10:42:32 -0300

    Fix TS issue

--- app/users/queries/getCurrentUser.ts
+++ app/users/queries/getCurrentUser.ts
@@ -5,7 +5,7 @@ export default async function getCurrentUser(_ = null, ctx: Ctx) {
   if (!ctx?.session?.userId) return null
 
   const user = await db.user.findFirst({
-    where: { id: ctx.session.userId },
+    where: { id: ctx.session.userId as string },
     select: { id: true, name: true, email: true, role: true, slackUserId: true, isInstalled: true },
   })
 
